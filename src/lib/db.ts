03c53795ca606cb59d8df947f2f02f37beca6a14
vyasanuj <vanuj616@gmail.com>
2025-02-20 19:48:25 +0530

    resolving deployment issue

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,17 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+// declare global {
+//   var prisma: PrismaClient | undefined;
+// }
 
-export const db = globalThis.prisma || new PrismaClient();
+// export const db = globalThis.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+// if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+
+import { PrismaClient } from '@prisma/client'
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
+
+export const db = globalForPrisma.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
